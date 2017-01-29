ac2fd1db31d3931bf58bdc14290623e6fbc17e37
Karimov Ildar <ildarkarimov22@gmail.com>
2017-01-29 16:36:30 +0500

    Созданы: первоначальные файлы, каркас приложения, основные react компоненты

---
+++ notes.js
@@ -0,0 +1,45 @@
+var Note = React.createClass({
+    render: function() {
+        return (
+            <div className="note"> Note </div>
+        );
+    }
+});
+
+var NoteEditor = React.createClass({
+    render: function() {
+        return (  
+            <div className="note-editor">NoteEditor</div>
+        );
+    }
+});
+
+var NotesGrid = React.createClass({
+    render: function() {
+        return (
+            <div className="notes-grid">
+                NotesGrid
+                <Note />
+                <Note />
+                <Note />
+            </div>
+        );
+    }
+});
+
+var NotesApp = React.createClass({
+    render: function() {
+        return (
+            <div>
+                NotesApp
+                <NoteEditor/>
+                <NotesGrid/>
+            </div>
+        );
+    },
+});
+
+ReactDOM.render(
+    <NotesApp />,
+    document.getElementById('mount-point')
+);
